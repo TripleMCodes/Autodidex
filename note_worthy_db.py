212ffe8089e46ba7faff56e957e8e39067319dec
TripleMCodes <khona6047@gmail.com>
2026-06-05 19:09:27 +0200

    add notebook and notes updates

--- note_worthy_db.py
+++ note_worthy_db.py
@@ -51,8 +51,43 @@ class Notes():
             logging.error(f"Error occurred while adding note: {e}")
             return {"message": "Failed to add note"}
 
-    def _check_note_book(self, name:str):
-        """check for the note book name in database"""
+    
+    def update_notebook_name(self, old_name:str, new_name:str):
+        """updates the names of the notebook"""
+        new_name_check = self._check_note_book(new_name)
+        old_name_check = self._check_note_book(old_name)
+
+        if not new_name_check and old_name_check:
+            query = f"""UPDATE {self.notebooks_table_name} SET name = ? WHERE name = ?;"""
+            try:
+                self.conn_cursor.execute(query, (new_name, old_name))
+                self._commit_data()
+                return {"message": "Notebook name updated successfully"}
+            except sqlite3.Error as e:
+                logging.error(f"Error occurred while updating notebook name: {e}")
+                return {"message": "Failed to update notebook name"}
+        else:
+            return {'message': "New notebook name already exists"}
+        
+    def update_note_content(self, notebook_name:str, note_title:str, new_content:str):
+        """updates the content of a note"""
+        notebook_id_query = f"""SELECT id FROM {self.notebooks_table_name} WHERE name = ?;"""
+        try:
+            self.conn_cursor.execute(notebook_id_query, (notebook_name,))
+            notebook_id = self.conn_cursor.fetchone()
+            if notebook_id is None:
+                return {"message": "Notebook not found"}
+            notebook_id = notebook_id[0]
+            update_note_query = f"""UPDATE {self.notes_table_name} SET content = ? WHERE notebook_id = ? AND title = ?;"""
+            self.conn_cursor.execute(update_note_query, (new_content, notebook_id, note_title))
+            self._commit_data()
+            return {"message": "Note content updated successfully"}
+        except sqlite3.Error as e:
+            logging.error(f"Error occurred while updating note content: {e}")
+            return {"message": "Failed to update note content"}
+
+    def _check_note_book(self, name:str) -> bool | sqlite3.Error:
+        """check for the note book name in database, returns True if notebook found"""
         query = f"""SELECT EXISTS(
                 SELECT 1
                 FROM {self.notebooks_table_name}
@@ -72,5 +107,7 @@ if __name__ == "__main__":
     print(notes._check_note_book("Test Notebook 2"))
     # created_notebook = notes.Insert_a_new_notebook("Test Notebook 2")
     # print(created_notebook)
-    added_note = notes.Insert_a_new_note("Test Notebook 2", "Test Note", "This is a test note.")
-    print(added_note)
+    # added_note = notes.Insert_a_new_note("Test Notebook 2", "Test Note", "This is a test note.")
+    # print(added_note)
+    # updated_notebook = notes.update_notebook_name("Test Notebook 2", "Updated Test Notebook")  
+    # print(updated_notebook)
